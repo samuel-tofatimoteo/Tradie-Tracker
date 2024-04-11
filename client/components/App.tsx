d119212cee7f0b79b3e57ea78edf9585f3f5ef9e
Michael Moll <michael.moll@developersinstitute.co.nz>
2024-04-12 11:25:04 +1200

    made manager-employee login button

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,9 +1,16 @@
-import { Outlet } from 'react-router-dom'
+import { Link, Outlet } from 'react-router-dom'
 
 function App() {
   return (
     <>
+      {/*  Navbar */}
       <div className="app">
+        <Link to="/manager/job-list">
+          <button className="manager-login-button">Manager</button>
+        </Link>
+        <Link to="/employee/schedule">
+          <button className="employee-login-button">Employee</button>
+        </Link>
         <Outlet />
       </div>
     </>
