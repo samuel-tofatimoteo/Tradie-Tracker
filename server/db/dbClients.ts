661f6ff64c53b322d6593c88a3d0ca771f1658b7
Sam <121822122+samuel-tofatimoteo@users.noreply.github.com>
2024-04-11 16:24:57 +1200

    Added client db functions

---
+++ server/db/dbClients.ts
@@ -0,0 +1,27 @@
+import { Client, Clients } from '../../models/clients'
+import connection from './connection'
+
+const db = connection
+
+// Client sign up account
+export async function addClient(clientSignUp: Client): Promise<Client> {
+  return await db('clients').insert(clientSignUp)
+}
+
+// Client account profile page
+export async function getClientById(id: number): Promise<Client> {
+  return await db('clients').where(id).select().first()
+}
+
+// Client account edit profile page
+export async function updateClient(
+  id: number,
+  updateData: Clients,
+): Promise<Client> {
+  return await db('clients').where(id).update(updateData)
+}
+
+// Client delete account
+export async function delClient(id: number) {
+  return await db('clients').where(id).del()
+}
