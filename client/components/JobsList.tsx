f3e0603169eaa25c00e4fff4cb3c68f1f79affba
Moa Ha <moahaa03@gmail.com>
2024-04-11 14:50:26 +1200

    jobs-job model made

---
+++ client/components/JobsList.tsx
@@ -0,0 +1,11 @@
+import { useJobs } from '../hooks/useJobs'
+
+function JobsList() {
+  return (
+    <>
+      <h1>test</h1>
+    </>
+  )
+}
+
+export default JobsList
--- models/fruit.ts
+++ models/fruit.ts
@@ -1,10 +1,18 @@
-export interface Fruit {
-  id: number
-  name: string
-  owner: string
+export interface Jobs {
+  title: string
+  description: string
+  location: string
+  date: string
+  time: string
+  complete: false
+  price: number
+  review: string
+  worked_hours: number
+  employee_id: number
+  client_id: number
+  manager_id: number
 }
 
-export interface FruitData {
-  name: string
-  owner: string
+export interface Job extends Jobs {
+  id: number
 }
