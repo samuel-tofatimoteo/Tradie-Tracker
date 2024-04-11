3c6b2d69b08e8dd29aaf20a3ca236edb1b0c6883
douglas <douglas_post93@hotmail.com>
2024-04-11 15:41:16 +1200

    created db function for companies

--- server/db/companies.ts
+++ server/db/companies.ts
@@ -1,3 +1,4 @@
+import { Companies } from '../../models/companies'
 import connection from './connection'
 import knexfile from './knexfile'
 import Knex from 'knex'
@@ -9,4 +10,14 @@ export async function getCompJobs() {
 }
 export async function addCompJobs() {
   return await db('jobs').insert('addCompJobs')
+}
+export async function deleteCompJobs() {
+  return await db('jobs').del()
+}
+export async function updateCompJobs() {
+  return await db('jobs').update('updateJobs')
+}
+export async function getCompJobsById(id: number | string) {
+  const company = await db('companies').select().first().where({ id })
+  return company as Companies
 }
