03be0118c0ec5552dc4c8067a9d365b7b7c8badd
Moa Ha <moahaa03@gmail.com>
2024-04-12 16:44:20 +1200

    start Navbar

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,9 +1,10 @@
 import { Outlet } from 'react-router-dom'
+import Navbar from './Nav'
 
 function App() {
   return (
     <>
-      {/*  Navbar */}
+      <Navbar />
       <div className="app">
         <Outlet />
       </div>
---
+++ client/components/Navbar.tsx
@@ -0,0 +1,11 @@
+function Navbar() {
+  return (
+    <>
+      <div className="bg-color">
+        <h1 className="h1-color">Tradie Tracker</h1>
+      </div>
+    </>
+  )
+}
+
+export default Navbar
