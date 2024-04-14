bdbdbe4d25674cc7d1ffdd72a9c96743a6dbea69
Michael Moll <michael.moll@developersinstitute.co.nz>
2024-04-15 11:26:10 +1200

    basic createJob page setup

--- client/components/CreateJob.tsx
+++ client/components/CreateJob.tsx
@@ -1,37 +1,92 @@
+import { useState } from 'react'
+
 function CreateJob() {
+  const [occuringClient, setOccuringClient] = useState(false)
+
+  const handleCheckboxChange = (event) => {
+    setOccuringClient(event.target.checked)
+  }
+
   return (
     <>
-      <h1 className="create-job-title">Create job</h1>
-      {/* Title */}
+      {/* Job Title */}
       <form>
         <label>
-          Title
-          <input placeholder="E.g: Fixing light bulb" />
+          Job Title
+          <input placeholder="E.g: Software Developer" />
         </label>
       </form>
-      {/* Description */}
+      {/* Job Description */}
       <form>
         <label>
-          Description
-          <input placeholder="E.g: On the second floor, storage room. The main lightbulb is broken. Needs replacing." />
+          Job Description
+          <textarea placeholder="E.g: Develop software applications." />
         </label>
       </form>
-      {/* Date and Time */}
+      {/* Date & time */}
       <form>
         <label>
-          Date and Time
-          <input placeholder="E.g: 20/04/2024" />
+          Date & Time
+          <input type="datetime-local" />
         </label>
       </form>
-      {/* Occurring CLient */}
+      {/* Occurring Client */}
       <label>
-        Occurring Client? <input type="checkbox" name="Occurring Client" />
+        Occurring Client?
+        <input
+          type="checkbox"
+          name="Occurring Client"
+          checked={occuringClient}
+          onChange={handleCheckboxChange}
+        />
       </label>
       {/* Occurring Client TRUE */}
-      <select>
-        <option value="someOption">Company</option>
-        <option value="otherOption">Other option</option>
-      </select>
+      {occuringClient && (
+        <select>
+          <option value="someOption">Company</option>
+          <option value="otherOption">Other option</option>
+        </select>
+      )}
+      {/* Occurring Client FALSE */}
+      {!occuringClient && (
+        <>
+          {/* Company Name */}
+          <form>
+            <label>
+              Company Name
+              <input placeholder="E.g: Costco" />
+            </label>
+          </form>
+          {/* Address */}
+          <form>
+            <label>
+              Address
+              <input placeholder="E.g: 1234 Main St, Vancouver, BC" />
+            </label>
+          </form>
+          {/* Contact Name */}
+          <label>
+            Contact Name
+            <input placeholder="E.g: John Doe" />
+          </label>
+          {/* Contact Phone */}
+          <form>
+            <label>
+              Contact Phone
+              <input placeholder="E.g: 123-456-7890" />
+            </label>
+          </form>
+          {/* Contact Email */}
+          <form>
+            <label>
+              Contact Email
+              <input placeholder="E.g: john.doe@example.com" type="email" />
+            </label>
+          </form>
+          {/* Submit button */}
+          <button>Submit</button>
+        </>
+      )}
     </>
   )
 }
