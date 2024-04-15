b7c0e46ac70f75c5ca9c1a8c6b757607f67a91aa
douglas <douglas_post93@hotmail.com>
2024-04-15 14:46:19 +1200

    login page Created

---
+++ client/components/EmployeeLogin.tsx
@@ -0,0 +1,61 @@
+import { FormEvent, SetStateAction, useState } from 'react'
+import { Link, Outlet } from 'react-router-dom'
+
+function EmployeeLogin() {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleUsernameChange = (event: {
+    target: { value: SetStateAction<string> }
+  }) => {
+    setUsername(event.target.value)
+  }
+
+  const handlePasswordChange = (event: {
+    target: { value: SetStateAction<string> }
+  }) => {
+    setPassword(event.target.value)
+  }
+
+  const handleSubmit = (event: { preventDefault: () => void }) => {
+    event.preventDefault()
+  }
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={handleUsernameChange}
+            placeholder="Enter your username"
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={handlePasswordChange}
+            placeholder="Enter your password"
+            required
+          />
+        </div>
+        <Link to="/jobs/employee">
+          <button>Login</button>
+        </Link>
+      </form>
+      <p>
+        Do not have an account? <Link to="/signup">Sign up</Link>
+      </p>
+    </div>
+  )
+}
+
+export default EmployeeLogin
