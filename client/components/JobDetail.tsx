8fff40d2f52dc3b35c59bfee6736cf1d319f3197
Moa Ha <moahaa03@gmail.com>
2024-04-15 13:35:55 +1200

    commit before pull main

--- client/components/JobDetail.tsx
+++ client/components/JobDetail.tsx
@@ -1,17 +1,64 @@
-import { useState } from 'react'
+import { FormEvent, useState } from 'react'
 import { useEditJobById, useJobById } from '../hooks/useJobs'
 
-import { useParams, useSearchParams } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 
 function JobDetail() {
   const id = Number(useParams().id)
 
-  const { data } = useJobById(id)
+  const { data, isLoading, isError, error } = useJobById(id)
+  console.log(data)
+  // console.log(data.title)
+
   const mutation = useEditJobById()
-  const [formState, setFormState] = useState('')
+  const [formState, setFormState] = useState(
+    // '',
+    {
+      id: id,
+      data: {
+        title: data?.title,
+        description: data?.description,
+        location: data?.location,
+        date: data?.date,
+        time: data?.time,
+        complete: data?.complete,
+        price: data?.price,
+        worked_hours: data?.worked_hours,
+        employee_id: data?.employee_id,
+        client_id: data?.client_id,
+        manager_id: data?.manager_id,
+      },
+    },
+  )
 
   function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
     const { name, value } = e.currentTarget
+    const input = {
+      title: data?.title,
+      description: data?.description,
+      location: data?.location,
+      date: data?.date,
+      time: data?.time,
+      complete: data?.complete,
+      price: data?.price,
+      worked_hours: data?.worked_hours,
+      employee_id: data?.employee_id,
+      client_id: data?.client_id,
+      manager_id: data?.manager_id,
+    }
+    setFormState({ id, data: { ...input, [name]: value } })
+  }
+
+  if (isLoading) {
+    return <>is loading</>
+  }
+  if (isError) {
+    return <>error: {error}</>
+  }
+
+  function handleClick(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+    mutation.mutate(formState)
   }
   if (data) {
     return (
@@ -22,7 +69,7 @@ function JobDetail() {
             onChange={handleChange}
             value={formState}
             type="text"
-            name={data.id}
+            name="title"
             placeholder={data.title}
           />
           <br></br>
@@ -31,7 +78,7 @@ function JobDetail() {
             onChange={handleChange}
             value={formState}
             type="datetime-local"
-            name={data.id}
+            name="date"
             placeholder={data.date}
           />
           <br></br>
@@ -40,7 +87,7 @@ function JobDetail() {
             onChange={handleChange}
             value={formState}
             type="text"
-            name={data.id}
+            name="location"
             placeholder={data.location}
           />
           <br></br>
@@ -49,7 +96,7 @@ function JobDetail() {
             onChange={handleChange}
             value={formState}
             type="text"
-            name={data.id}
+            name="description"
             placeholder={data.description}
           />
           <br></br>
@@ -58,10 +105,11 @@ function JobDetail() {
             onChange={handleChange}
             value={formState}
             type="text"
-            name={data.id}
+            name="price"
             placeholder={data.price}
           />
         </form>
+        <button onClick={handleClick}>save</button>
       </>
     )
   }
