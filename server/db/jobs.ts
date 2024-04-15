b2b6ce0bb017a531a0c0db1a931b15558551a41b
douglas <douglas_post93@hotmail.com>
2024-04-15 13:27:37 +1200

    BE working

--- server/db/jobs.ts
+++ server/db/jobs.ts
@@ -3,6 +3,47 @@ import { Job, Jobs } from '../../models/jobs'
 
 const db = connection
 
+export async function getAllJobs() {
+  return await db('jobs').select()
+}
+export async function addJobs(data: Jobs) {
+  return await db('jobs').insert(data)
+}
+export async function deleteJobs(id: number) {
+  return await db('jobs').where('id', id).del()
+}
+export async function updateJobs(id: number, data: Jobs) {
+  return await db('jobs').where('id', id).update(data)
+}
+export async function getCompletedJobs() {
+  return await db('jobs').where('complete', true).select()
+}
+export async function getJob(jobId: number) {
+  return await db('jobs').where('id', jobId).select()
+}
+
+//employees
+export async function getAllJobsByEmpId(id: number) {
+  return await db('jobs').where('employee_id', id)
+}
+
+export async function getJobByEmpId(employeeId: number, jobId: number) {
+  return await db('jobs').where('employee_id', employeeId).where('id', jobId)
+}
+
+export async function updateJobByEmpId(
+  employeeId: number,
+  jobId: number,
+  data: Jobs,
+) {
+  return await db('jobs')
+    .where('employee_id', employeeId)
+    .where('id', jobId)
+    .update(data)
+}
+
+// OLD FUNCTIONS MIGHT DEL LATER :P
+
 export async function getManagersJob(managerId: number, jobId: number) {
   return await db('jobs')
     .where('manager_id', managerId)
@@ -17,34 +58,6 @@ export async function updateManagersJob(
 ) {
   return await db('jobs')
     .where('manager_id', managerId)
-    .where('id', jobId)
-    .update(data)
-}
-
-export async function getAllJobsByManager(id: number) {
-  return await db('jobs').select().where('manager_id', id)
-}
-export async function addJobs(data: Jobs) {
-  return await db('jobs').insert(data)
-}
-export async function deleteJobs(id: number) {
-  return await db('jobs').where('id', id).del()
-}
-export async function updateJobs(id: number, data: Jobs) {
-  return await db('jobs').where('id', id).update(data)
-}
-//employees
-export async function getJobByEmpId(id: number) {
-  return await db('jobs').select().where('employee_id', id)
-}
-
-export async function updateJobByEmpId(
-  employeeId: number,
-  jobId: number,
-  data: Jobs,
-) {
-  return await db('jobs')
-    .where('employee_id', employeeId)
     .where('id', jobId)
     .update(data)
 }
