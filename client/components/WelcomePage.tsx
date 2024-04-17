c3b735e9f62a9292e82518c94e3301c9429de38e
Michael Moll <michael.moll@developersinstitute.co.nz>
2024-04-18 08:03:55 +1200

    welcome-page complete

--- client/components/WelcomePage.tsx
+++ client/components/WelcomePage.tsx
@@ -3,19 +3,21 @@ import { Link, Outlet } from 'react-router-dom'
 function WelcomePage() {
   return (
     <>
-      <div className="header">
-        <h1 className="welcome-heading">Welcome to Tradie Tracker!</h1>
+      <div className="layout">
+        <div className="header">
+          <h1 className="welcome-heading">Welcome to Tradie Tracker!</h1>
+        </div>
         <h2 className="welcome-text">Hammering Out Efficiency</h2>
-      </div>
-      <h3 className="I-am-text">I am:</h3>
-      <div className="welcome">
-        <Link to="/jobs/manager">
-          <button className="manager login-button">Manager</button>
-        </Link>
-        <Link to="/jobs/employeeLogin">
-          <button className="employee login-button">Employee</button>
-        </Link>
-        <Outlet />
+        <h3 className="I-am-text">I am:</h3>
+        <div className="welcome">
+          <Link to="/jobs/manager">
+            <button className="manager login-button">Manager</button>
+          </Link>
+          <Link to="/jobs/employeeLogin">
+            <button className="employee login-button">Employee</button>
+          </Link>
+          <Outlet />
+        </div>
       </div>
     </>
   )
